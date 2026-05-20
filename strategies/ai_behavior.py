2c6e44abc0513b71dda68780b627ca55a8e1feda
Simon-Struggler <sema.patyukov.84@mail.ru>
2026-05-20 17:22:13 +0300

    aggression variance

--- strategies/ai_behavior.py
+++ strategies/ai_behavior.py
@@ -13,9 +13,9 @@ class PassiveAI(AIBehavior):
         from actions.commands import MoveCommand
         return MoveCommand(entity, dx, dy, game_map, player, enemies)
 
-class SimpleAggressiveAI(AIBehavior):
+class MediumAggressiveAI(AIBehavior):
     def __init__(self):
-        self.is_awake = False # Спит, пока не откроют дверь!
+        self.is_awake = False
 
     def get_action(self, entity, game_map, player, enemies):
         if not self.is_awake:
@@ -27,6 +27,34 @@ class SimpleAggressiveAI(AIBehavior):
         distance = max(dx_dist, dy_dist)
         
         if distance <= 5:
+            step_dx = 0
+            if player.x > entity.x: step_dx = 1
+            elif player.x < entity.x: step_dx = -1
+            
+            step_dy = 0
+            if player.y > entity.y: step_dy = 1
+            elif player.y < entity.y: step_dy = -1
+            
+            from actions.commands import MoveCommand
+            return MoveCommand(entity, step_dx, step_dy, game_map, player, enemies)
+        else:
+            from actions.commands import WaitCommand
+            return WaitCommand()
+        
+class ShortAggressiveAI(AIBehavior):
+    def __init__(self):
+        self.is_awake = False
+
+    def get_action(self, entity, game_map, player, enemies):
+        if not self.is_awake:
+            from actions.commands import WaitCommand
+            return WaitCommand()
+
+        dx_dist = abs(player.x - entity.x)
+        dy_dist = abs(player.y - entity.y)
+        distance = max(dx_dist, dy_dist)
+        
+        if distance <= 3:
             step_dx = 0
             if player.x > entity.x: step_dx = 1
             elif player.x < entity.x: step_dx = -1
